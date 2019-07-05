bd0222d2f99bf06b4509eb38b8d6d82fb152c086
Kamil Nowak <kamil.nowak.jarocin@gmail.com>
2019-07-05 15:28:24 +0200

    Settings

--- pizza/settings.py
+++ pizza/settings.py
@@ -146,13 +146,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Mail
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
-
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
